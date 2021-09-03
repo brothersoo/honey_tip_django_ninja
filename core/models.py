0f6a6c0fefd7a2a820c533043e3f3289c7ff7f73
brothersoo <moistybro@gmail.com>
2021-09-03 20:36:58 +0900

    FIX: implicit base model an abc

--- core/models.py
+++ core/models.py
@@ -2,7 +2,15 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 
-class TimeStampedModel(models.Model):
+class BaseModel(models.Model):
+
+    objects = models.Manager()
+
+    class Meta:
+        abstract = True
+
+
+class TimeStampedModel(BaseModel):
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
     updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
